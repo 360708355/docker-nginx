4afc8a55e089f526aac0feb33986083e364c7627
kazii <443915662@qq.com>
2017-03-16 16:37:51 +0800

    fix

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ then
 	echo "docker build success"
 fi
 
-if docker run -dit -p 80:80 -p 443:443 -p 3001:3001 -p 6379:6379 kazii/nginx
+if docker run -dit -p 80:80 -p 443:443 -p 3001:3001 kazii/nginx
 
 then
 	echo "docker run success"
